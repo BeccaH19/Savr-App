91471ca172d9be8639cfb2ea706238fef2e40bc9
BeccaH19 <rebeccahall203@gmail.com>
2025-04-10 22:06:25 -0700

    made login as the launcher so running it works

--- app/src/main/java/com/example/savr/MainActivity.kt
+++ app/src/main/java/com/example/savr/MainActivity.kt
@@ -1,10 +1,13 @@
 package com.example.savr
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.savr.activities.LoginActivity
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,5 +19,7 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        startActivity(Intent(this, LoginActivity::class.java))
+        finish()
     }
 }
